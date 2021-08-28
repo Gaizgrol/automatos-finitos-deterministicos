b0515e16880cbc89b448da6f497db280f4977bef
Gaizgrol <gabrielizotongo@gmail.com>
2021-08-28 01:32:02 -0300

    💡 Comentários na classe Estado, 🔨 Encapsulamento de dados na classe Estado

--- src/classes/Estado.ts
+++ src/classes/Estado.ts
@@ -1,7 +1,8 @@
 export default class Estado
 {
-    id: string;
-    mapa: Map<string, Estado>;
+    // ID não deve ser alterado durante execução
+    private readonly id: string;
+    private mapa: Map<string, Estado>;
 
     constructor( id: string )
     {
@@ -9,6 +10,13 @@ export default class Estado
         this.mapa = new Map();
     }
 
+    /**
+     * Troca uma entrada (símbolo) que já estava mapeada para um estado de saída
+     * 
+     * @param simboloAnterior Símbolo que será substituído
+     * @param novoSimbolo Símbolo novo, entrará no lugar do anterior
+     * @returns 
+     */
     trocaEntrada( simboloAnterior: string, novoSimbolo: string ): boolean
     {
         const estado = this.estado( simboloAnterior );
@@ -30,16 +38,35 @@ export default class Estado
         return false;
     }
 
+    /**
+     * Atualiza um mapeamento (Símbolo → Estado) ou insere um novo caso não exista
+     * 
+     * @param simbolo 
+     * @param estado 
+     */
     atualiza( simbolo: string, estado: Estado ): void
     {
         this.mapa.set( simbolo, estado );
     }
 
+    /**
+     * Encontra o Estado mapeado de acordo com o Símbolo passado
+     * 
+     * @param simbolo Símbolo de entrada
+     * @returns Estado mapeado. Caso não exista, retorna null
+     */
     estado( simbolo: string ): Estado
     {
-        return this.mapa.get( simbolo );
+        return this.mapa.get( simbolo ) ?? null;
     }
 
+
+    /**
+     * Parecido com o método `estado`, porém quando não há mapeamento retorna uma referência própria
+     * 
+     * @param simbolo Símbolo de entrada
+     * @returns Estado mapeado. Caso não exista, retorna uma referência própria
+     */
     transicao( simbolo: string ): Estado
     {
         // Caso não tenha mapeamento, continue no estado atual
