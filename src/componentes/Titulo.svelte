940df70041ec53d934db5bbd05f5c5224296e0c6
Gaizgrol <gabrielizotongo@gmail.com>
2021-08-29 08:53:30 -0300

    💡 Alguns comentários

--- src/componentes/Titulo.svelte
+++ src/componentes/Titulo.svelte
@@ -1,31 +1,38 @@
 <h1>
+    <!--Palavras 'piscando' como se fosse um autômato-->
     {#each palavras as palavra, i}
-        <span class="suave {i === palavras.length-1 ? 'italico' : 'titulo'} { palavraSelecionada === i ? 'selecionado' : '' }">{palavra}</span>
+        <span
+            class="suave {i === palavras.length-1 ? 'italico' : 'titulo'} { palavraSelecionada === i ? 'selecionado' : '' }"
+        >
+            {palavra}
+        </span>
     {/each}
 </h1>
-<h2>
-
-</h2>
 
 <script context="module" lang="ts">
 
-    import { inteiroAleatorio, numeroAleatorio } from '../auxiliares/RandomUtils'
+    // Outros
+    import { inteiroAleatorio } from '../auxiliares/RandomUtils'
 
 </script>
-
 <script lang="ts">
 
-    const frase: string = 'Autômatos Finitos Determinísticos: Playground';
-    const palavras: string[] = frase.split(' ');
+    // Frase que piscará
+    const palavras: string[] = 'Autômatos Finitos Determinísticos: Playground'.split(' ');
+
+    // Índice da palavra
     let palavraSelecionada: number = inteiroAleatorio( 0, palavras.length );
 
     setInterval(() =>
     {
+        // Não acende a mesma palavra
         let anterior = palavraSelecionada;
         do
         {
             palavraSelecionada = inteiroAleatorio( 0, palavras.length );
-        } while( palavraSelecionada === anterior )
+        }
+        while( palavraSelecionada === anterior )
+        
     }, 1000 );
 
 </script>
