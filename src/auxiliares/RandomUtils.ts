5c4f799f710a94d984c1627ff75647c7f11d0c9b
Gaizgrol <gabrielizotongo@gmail.com>
2021-08-29 09:37:49 -0300

    Updates

--- src/auxiliares/RandomUtils.ts
+++
@@ -1,3 +0,0 @@
-export const numeroAleatorio = ( min: number, max: number ): number => min + Math.random() * ( max - min );
-export const inteiroAleatorio = ( min: number, max: number ): number => Math.trunc( numeroAleatorio( min, max ) );
-export const elementoAleatorio = ( elemento: string | Array<any> ): ( string | any ) => elemento[ inteiroAleatorio( 0, elemento.length ) ];
